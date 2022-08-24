b8e139f105a130ec526da7dac8baab0b8a7c804c
Apoorv Jain <apoorv_jain@external.mckinsey.com>
2022-08-24 17:07:48 +0530

    button change

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,48 +2,48 @@ import React from "react";
 import "./Button.css";
 
 export interface ButtonProps {
-  /**
-   * How the button should look
-   */
-  variant?: "outlined" | "contained" | "text";
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "sm" | "md" | "lg";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+    /**
+     * How the button should look
+     */
+    variant?: "outlined" | "contained" | "text";
+    /**
+     * What background color to use
+     */
+    backgroundColor?: string;
+    /**
+     * How large should the button be?
+     */
+    size?: "sm" | "md" | "lg";
+    /**
+     * Button contents
+     */
+    label: string;
+    /**
+     * Optional click handler
+     */
+    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
 /**
  * Primary UI component for user interaction
  */
 const Button = ({
-  backgroundColor,
-  size = "md",
-  variant = "contained",
-  onClick,
-  label,
+    backgroundColor,
+    size = "md",
+    variant = "contained",
+    onClick,
+    label,
 }: ButtonProps) => {
-  return (
-    <button
-      type='button'
-      className={["btn", `btn--${size}`, `btn--${variant}`].join(" ")}
-      style={backgroundColor ? { backgroundColor } : {}}
-      onClick={onClick}
-    >
-      Hello, {label}
-    </button>
-  );
+    return (
+        <button
+            type="button"
+            className={["btn", `btn--${size}`, `btn--${variant}`].join(" ")}
+            style={backgroundColor ? { backgroundColor } : {}}
+            onClick={onClick}
+        >
+            Hello User, {label}
+        </button>
+    );
 };
 
 export default Button;
